5c58eb11c134860aaa054813a9bdd08b2a5c9291
Adrian Firmansyah <adrian@firmansyah.co>
2026-09-01 16:53:53 +0700

    chore(release): bump archguard version to 0.3.1

--- src/archguard/__init__.py
+++ src/archguard/__init__.py
@@ -3,5 +3,5 @@ ArchGuard Core Governance Engine.
 Enterprise architecture and standards compliance for modern software systems.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Firmansyah Consulting & Enterprise Systems"
